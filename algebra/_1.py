b889b7887ab833582874f8a5829db80b7a094f0a
Marcus <88283829+PyMarcus@users.noreply.github.com>
2022-05-08 15:53:26 -0300

    Add files via upload

---
+++ algebra/_1.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    inteiro = int(str(11111101), 2)
+    inteiro2 = int(str('FD'), 16)
+    print(inteiro2)
+    print(inteiro)
+    print(hex(inteiro))
+    print(bin(inteiro))
